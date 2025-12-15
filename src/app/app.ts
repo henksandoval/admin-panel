e70c44b866159dcebdc65f00bd884d1fcb45b634
Henk Sandoval <henk.sandoval@outlook.com>
2025-12-15 05:39:13 +0100

    style: 🎨 Add host classes for full height and width to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  host: {
+    class: 'block h-full w-full'
+  }
 })
 export class App {
 }
