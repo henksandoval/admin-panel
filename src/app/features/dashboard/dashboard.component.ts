6bf2f8a962c6de71cf9e7b634da0266dbdc2d528
Henk Sandoval <henk.sandoval@outlook.com>
2025-12-31 01:10:37 +0100

    style: 🎨 Refactor theme system and mixins for improved clarity and performance

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [
+    MatButton
+  ],
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
