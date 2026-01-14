355c6996ae48ce77c5f423e0f8c0be0e84e871d2
Henk Sandoval <henk.sandoval@outlook.com>
2026-01-14 18:21:29 +0100

    feat: ✨ Remove unused button examples and imports from dashboard component

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { ThemePalette } from '@angular/material/core';
-import {MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from '@angular/material/button';
-import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    MatButton,
-    MatIcon,
-    MatFabButton,
-    MatIconButton,
-    MatMiniFabButton
-  ],
+  imports: [],
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
