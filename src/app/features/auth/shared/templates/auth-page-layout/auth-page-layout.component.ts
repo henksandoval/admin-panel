e1214a4fc63a7daa676c1890c6a84be4f5f345cc
Henk Sandoval <henk.sandoval@outlook.com>
2026-03-11 22:09:20 +0100

    refactor(auth-page-layout): enhance box-shadow effects and restructure template for improved layout

--- src/app/features/auth/shared/templates/auth-page-layout/auth-page-layout.component.ts
+++ src/app/features/auth/shared/templates/auth-page-layout/auth-page-layout.component.ts
@@ -7,17 +7,19 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatIconModule],
   styleUrl: './auth-page-layout.component.scss',
   template: `
-    <div class="app-auth-page-layout__header">
-      <div class="app-auth-page-layout__header-inner flex items-end justify-between">
-        <div class="app-auth-page-layout__brand flex flex-col items-start pb-4 pl-8">
-          <span class="app-auth-page-layout__brand-name">Admin</span>
-          <span class="app-auth-page-layout__brand-sub mat-body-medium">Panel</span>
+    <div class="app-auth-page-layout__wrapper">
+      <div class="app-auth-page-layout__header">
+        <div class="app-auth-page-layout__header-inner flex items-end justify-between">
+          <div class="app-auth-page-layout__brand flex flex-col items-start pb-4 pl-8">
+            <span class="app-auth-page-layout__brand-name">Admin</span>
+            <span class="app-auth-page-layout__brand-sub mat-body-medium">Panel</span>
+          </div>
         </div>
+        <div class="app-auth-page-layout__header-accent"></div>
       </div>
-      <div class="app-auth-page-layout__header-accent"></div>
-    </div>
-    <div class="app-auth-page-layout__card">
-      <ng-content/>
+      <div class="app-auth-page-layout__card">
+        <ng-content/>
+      </div>
     </div>
   `,
 })
